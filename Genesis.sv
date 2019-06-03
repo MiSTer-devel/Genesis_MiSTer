00efa11ff06b22a7f3709e2ec487b4775a28a8ca
sorgelig <pour.garbage@gmail.com>
2019-06-03 16:29:34 +0800

    Option to detect region by ROM header.

--- Genesis.sv
+++ Genesis.sv
@@ -125,8 +125,8 @@ assign {UART_RTS, UART_TXD, UART_DTR} = 0;
 assign {SD_SCK, SD_MOSI, SD_CS} = 'Z;
 assign {SDRAM_DQ, SDRAM_A, SDRAM_BA, SDRAM_CLK, SDRAM_CKE, SDRAM_DQML, SDRAM_DQMH, SDRAM_nWE, SDRAM_nCAS, SDRAM_nRAS, SDRAM_nCS} = 'Z;
 
-assign VIDEO_ARX = status[9] ? 8'd16 : 8'd4;
-assign VIDEO_ARY = status[9] ? 8'd9  : 8'd3;
+assign VIDEO_ARX = status[10] ? 8'd16 : 8'd4;
+assign VIDEO_ARY = status[10] ? 8'd9  : 8'd3;
 
 assign AUDIO_S = 1;
 assign AUDIO_MIX = 0;
@@ -144,7 +144,8 @@ localparam CONF_STR = {
 	"FS,BINGENMD ;",
 	"-;",
 	"O67,Region,JP,US,EU;",
-	"O8,Auto Region,No,Yes;",
+	"O89,Auto Region,File Ext,Header,Disabled;",
+	"D2ORS,Priority,US>EU>JP,EU>US>JP,US>JP>EU,JP>US>EU;",
 	"-;",
 	"C,Cheats;",
 	"H1OO,Cheats Enabled,Yes,No;",
@@ -153,16 +154,17 @@ localparam CONF_STR = {
 	"D0RH,Save Backup RAM;",
 	"D0OD,Autosave,No,Yes;",
 	"-;",
-	"O9,Aspect ratio,4:3,16:9;",
+	"OA,Aspect ratio,4:3,16:9;",
 	"O13,Scandoubler Fx,None,HQ2x,CRT 25%,CRT 50%,CRT 75%;",
+	"-;",
+	"OEF,Audio Filter,Model 1,Model 2,Minimal,No Filter;",
+	"ON,HiFi PCM,No,Yes;",
 	"-;",
 	"O4,Swap Joysticks,No,Yes;",
 	"O5,6 Buttons Mode,No,Yes;",
 	"OLM,Multitap,Disabled,4-Way,TeamPlayer,J-Cart;",
 	"OIJ,Mouse,None,Port1,Port2;",
 	"OK,Mouse Flip Y,No,Yes;",
-	"OEF,Audio Filter,Model 1,Model 2,Minimal,No Filter;",
-   "ON,HiFi PCM,No,Yes;",
 	"-;",
    "OPQ,CPU Turbo,None,Medium,High;",
 	"-;",
@@ -176,7 +178,7 @@ localparam CONF_STR = {
 };
 
 
-wire [15:0] status_menumask = {~gg_available,~bk_ena};
+wire [15:0] status_menumask = {~status[8],~gg_available,~bk_ena};
 wire [31:0] status;
 wire  [1:0] buttons;
 wire [11:0] joystick_0,joystick_1,joystick_2,joystick_3;
@@ -490,7 +492,7 @@ reg        region_set = 0;
 wire       pressed = ps2_key[9];
 wire [8:0] code    = ps2_key[8:0];
 always @(posedge clk_sys) begin
-	reg old_state, old_download = 0;
+	reg old_state, old_ready = 0;
 	old_state <= ps2_key[10];
 
 	if(old_state != ps2_key[10]) begin
@@ -501,10 +503,62 @@ always @(posedge clk_sys) begin
 		endcase
 	end
 
+	old_ready <= cart_hdr_ready;
+	if(~status[9] & ~old_ready & cart_hdr_ready) begin
+		if(status[8]) begin
+			region_set <= 1;
+			case(status[28:27])
+				0: if(hdr_u) region_req <= 1;
+					else if(hdr_e) region_req <= 2;
+					else if(hdr_j) region_req <= 0;
+					else region_req <= 1;
+				
+				1: if(hdr_e) region_req <= 2;
+					else if(hdr_u) region_req <= 1;
+					else if(hdr_j) region_req <= 0;
+					else region_req <= 2;
+				
+				2: if(hdr_u) region_req <= 1;
+					else if(hdr_j) region_req <= 0;
+					else if(hdr_e) region_req <= 2;
+					else region_req <= 1;
+
+				3: if(hdr_j) region_req <= 0;
+					else if(hdr_u) region_req <= 1;
+					else if(hdr_e) region_req <= 2;
+					else region_req <= 0;
+			endcase
+		end
+		else begin
+			region_set <= |ioctl_index;
+			region_req <= ioctl_index[7:6];
+		end
+	end
+
+	if(old_ready & ~cart_hdr_ready) region_set <= 0;
+end
+
+reg cart_hdr_ready = 0;
+reg hdr_j=0,hdr_u=0,hdr_e=0;
+always @(posedge clk_sys) begin
+	reg old_download;
 	old_download <= cart_download;
-	if(status[8] & (old_download ^ cart_download) & |ioctl_index) begin
-		region_set <= cart_download;
-		region_req <= ioctl_index[7:6];
+
+	if(~old_download && cart_download) {hdr_j,hdr_u,hdr_e} <= 0;
+	if(old_download && ~cart_download) cart_hdr_ready <= 0;
+
+	if(ioctl_wr & cart_download) begin
+		if(ioctl_addr == 'h1F0 || ioctl_addr == 'h1F2) begin
+			if(ioctl_data[7:0] == "J") hdr_j <= 1;
+			else if(ioctl_data[7:0] == "U") hdr_u <= 1;
+			else if(ioctl_data[7:0] >= "0" && ioctl_data[7:0] <= "Z") hdr_e <= 1;
+		end
+		if(ioctl_addr == 'h1F0) begin
+			if(ioctl_data[15:8] == "J") hdr_j <= 1;
+			else if(ioctl_data[15:8] == "U") hdr_u <= 1;
+			else if(ioctl_data[15:8] >= "0" && ioctl_data[7:0] <= "Z") hdr_e <= 1;
+		end
+		if(ioctl_addr == 'h200) cart_hdr_ready <= 1;
 	end
 end
 
@@ -516,39 +570,40 @@ reg pier_quirk = 0;
 reg ttn2_quirk = 0;
 reg svp_quirk = 0;
 always @(posedge clk_sys) begin
-	reg [55:0] cart_id;
-	reg old_download, old_reset;
+	reg [63:0] cart_id;
+	reg old_download;
 	old_download <= cart_download;
 
 	if(~old_download && cart_download) {fifo_quirk,eeprom_quirk,sram_quirk,noram_quirk,pier_quirk,ttn2_quirk,svp_quirk} <= 0;
 
 	if(ioctl_wr & cart_download) begin
-		if(ioctl_addr == 'h182) cart_id[55:48] <= {ioctl_data[15:8]};
-		if(ioctl_addr == 'h184) cart_id[47:32] <= {ioctl_data[7:0],ioctl_data[15:8]};
-		if(ioctl_addr == 'h186) cart_id[31:16] <= {ioctl_data[7:0],ioctl_data[15:8]};
-		if(ioctl_addr == 'h188) cart_id[15:00] <= {ioctl_data[7:0],ioctl_data[15:8]};
-		if(ioctl_addr == 'h18A) begin
-			     if({cart_id,ioctl_data[7:0]} == "T-081276") sram_quirk <= 1;   // NFL Quarterback Club
-			else if({cart_id,ioctl_data[7:0]} == "T-81406 ") sram_quirk <= 1;   // NBA Jam TE
-			else if({cart_id,ioctl_data[7:0]} == "T-081586") sram_quirk <= 1;   // NFL Quarterback Club '96
-			else if({cart_id,ioctl_data[7:0]} == "T-81576 ") sram_quirk <= 1;   // College Slam
-			else if({cart_id,ioctl_data[7:0]} == "T-81476 ") sram_quirk <= 1;   // Frank Thomas Big Hurt Baseball
-			else if({cart_id,ioctl_data[7:0]} == "MK-1215 ") eeprom_quirk <= 1; // Evander Real Deal Holyfield's Boxing
-			else if({cart_id,ioctl_data[7:0]} == "G-4060  ") eeprom_quirk <= 1; // Wonder Boy
-			else if({cart_id,ioctl_data[7:0]} == "00001211") eeprom_quirk <= 1; // Sports Talk Baseball
-			else if({cart_id,ioctl_data[7:0]} == "MK-1228 ") eeprom_quirk <= 1; // Greatest Heavyweights
-			else if({cart_id,ioctl_data[7:0]} == "G-5538  ") eeprom_quirk <= 1; // Greatest Heavyweights JP
-			else if({cart_id,ioctl_data[7:0]} == "00004076") eeprom_quirk <= 1; // Honoo no Toukyuuji Dodge Danpei
-			else if({cart_id,ioctl_data[7:0]} == "T-12046 ") eeprom_quirk <= 1; // Mega Man - The Wily Wars 
-			else if({cart_id,ioctl_data[7:0]} == "T-12053 ") eeprom_quirk <= 1; // Rockman Mega World 
-			else if({cart_id,ioctl_data[7:0]} == "G-4524  ") eeprom_quirk <= 1; // Ninja Burai Densetsu
-			else if({cart_id,ioctl_data[7:0]} == "T-113016") noram_quirk <= 1;  // Puggsy fake ram check
-			else if({cart_id,ioctl_data[7:0]} == "T-89016 ") fifo_quirk <= 1;   // Clue
-			else if({cart_id,ioctl_data[7:0]} == "T-574023") pier_quirk <= 1;   // Pier Solar Reprint
-			else if({cart_id,ioctl_data[7:0]} == "T-574013") pier_quirk <= 1;   // Pier Solar 1st Edition
-			else if({cart_id,ioctl_data[7:0]} == "TITAN002") ttn2_quirk <= 1;   // Titan Overdrive 2
-			else if({cart_id,ioctl_data[7:0]} == "MK-1229 ") svp_quirk <= 1;    // Virtua Racing EU/US
-			else if({cart_id,ioctl_data[7:0]} == "G-7001  ") svp_quirk <= 1;    // Virtua Racing JP
+		if(ioctl_addr == 'h182) cart_id[63:56] <= ioctl_data[15:8];
+		if(ioctl_addr == 'h184) cart_id[55:40] <= {ioctl_data[7:0],ioctl_data[15:8]};
+		if(ioctl_addr == 'h186) cart_id[39:24] <= {ioctl_data[7:0],ioctl_data[15:8]};
+		if(ioctl_addr == 'h188) cart_id[23:08] <= {ioctl_data[7:0],ioctl_data[15:8]};
+		if(ioctl_addr == 'h18A) cart_id[07:00] <= ioctl_data[7:0];
+		if(ioctl_addr == 'h18C) begin
+			     if(cart_id == "T-081276") sram_quirk   <= 1; // NFL Quarterback Club
+			else if(cart_id == "T-81406 ") sram_quirk   <= 1; // NBA Jam TE
+			else if(cart_id == "T-081586") sram_quirk   <= 1; // NFL Quarterback Club '96
+			else if(cart_id == "T-81576 ") sram_quirk   <= 1; // College Slam
+			else if(cart_id == "T-81476 ") sram_quirk   <= 1; // Frank Thomas Big Hurt Baseball
+			else if(cart_id == "MK-1215 ") eeprom_quirk <= 1; // Evander Real Deal Holyfield's Boxing
+			else if(cart_id == "G-4060  ") eeprom_quirk <= 1; // Wonder Boy
+			else if(cart_id == "00001211") eeprom_quirk <= 1; // Sports Talk Baseball
+			else if(cart_id == "MK-1228 ") eeprom_quirk <= 1; // Greatest Heavyweights
+			else if(cart_id == "G-5538  ") eeprom_quirk <= 1; // Greatest Heavyweights JP
+			else if(cart_id == "00004076") eeprom_quirk <= 1; // Honoo no Toukyuuji Dodge Danpei
+			else if(cart_id == "T-12046 ") eeprom_quirk <= 1; // Mega Man - The Wily Wars 
+			else if(cart_id == "T-12053 ") eeprom_quirk <= 1; // Rockman Mega World 
+			else if(cart_id == "G-4524  ") eeprom_quirk <= 1; // Ninja Burai Densetsu
+			else if(cart_id == "T-113016") noram_quirk  <= 1; // Puggsy fake ram check
+			else if(cart_id == "T-89016 ") fifo_quirk   <= 1; // Clue
+			else if(cart_id == "T-574023") pier_quirk   <= 1; // Pier Solar Reprint
+			else if(cart_id == "T-574013") pier_quirk   <= 1; // Pier Solar 1st Edition
+			else if(cart_id == "TITAN002") ttn2_quirk   <= 1; // Titan Overdrive 2
+			else if(cart_id == "MK-1229 ") svp_quirk    <= 1; // Virtua Racing EU/US
+			else if(cart_id == "G-7001  ") svp_quirk    <= 1; // Virtua Racing JP
 		end
 	end
 end
@@ -564,6 +619,7 @@ wire bk_change;
 
 always @(posedge clk_sys) begin
 	reg old_downloading = 0;
+	reg old_change = 0;
 
 	old_downloading <= downloading;
 	if(~old_downloading & downloading) bk_ena <= 0;
@@ -571,14 +627,13 @@ always @(posedge clk_sys) begin
 	//Save file always mounted in the end of downloading state.
 	if(downloading && img_mounted && !img_readonly) bk_ena <= 1;
 
-	if (bk_change & ~OSD_STATUS)
-		sav_pending <= 1'b1;
-	else if (bk_state)
-		sav_pending <= 1'b0;
+	old_change <= bk_change;
+	if (~old_change & bk_change & ~OSD_STATUS) sav_pending <= status[13];
+	else if (bk_state) sav_pending <= 0;
 end
 
 wire bk_load    = status[16];
-wire bk_save    = status[17] | (sav_pending & OSD_STATUS & status[13]);
+wire bk_save    = status[17] | (sav_pending & OSD_STATUS);
 reg  bk_loading = 0;
 reg  bk_state   = 0;
 
