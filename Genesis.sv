5fafcc39998018bbbb2a3c84c6101d4861cac461
sorgelig <pour.garbage@gmail.com>
2019-10-24 01:21:05 +0800

    fix shift/cut in blending module.

--- Genesis.sv
+++ Genesis.sv
@@ -501,19 +501,28 @@ always @(posedge CLK_VIDEO) old_ce_pix <= ce_pix;
 wire [7:0] red, green, blue;
 
 cofi coffee (
-	.clk(CLK_VIDEO),
-	.pix_ce(~old_ce_pix & ce_pix),
+	.clk(clk_sys),
+	.pix_ce(ce_pix),
 	.enable(status[34]),
-	.blank(hblank | vblank),
 
+	.hblank(hblank),
+	.vblank(vblank),
+	.hs(hs),
+	.vs(vs),
 	.red(color_lut[r]),
 	.green(color_lut[g]),
 	.blue(color_lut[b]),
 
+	.hblank_out(hblank_c),
+	.vblank_out(vblank_c),
+	.hs_out(hs_c),
+	.vs_out(vs_c),
 	.red_out(red),
 	.green_out(green),
 	.blue_out(blue)
 );
+
+wire hs_c,vs_c,hblank_c,vblank_c;
 
 video_mixer #(.LINE_LENGTH(320), .HALF_DEPTH(0), .GAMMA(1)) video_mixer
 (
@@ -534,10 +543,10 @@ video_mixer #(.LINE_LENGTH(320), .HALF_DEPTH(0), .GAMMA(1)) video_mixer
 	.B(blue),
 
 	// Positive pulses.
-	.HSync(hs),
-	.VSync(vs),
-	.HBlank(hblank),
-	.VBlank(vblank)
+	.HSync(hs_c),
+	.VSync(vs_c),
+	.HBlank(hblank_c),
+	.VBlank(vblank_c)
 );
 
 ///////////////////////////////////////////////////
--- cofi.sv
+++ cofi.sv
@@ -1,15 +1,22 @@
 // Composite-like horizontal blending by Kitrinx
 
 module cofi (
-	input clk,
-	input pix_ce,
-	input enable,
+	input        clk,
+	input        pix_ce,
+	input        enable,
 
-	input blank,
-	input [7:0] red,
-	input [7:0] green,
-	input [7:0] blue,
+	input        hblank,
+	input        vblank,
+	input        hs,
+	input        vs,
+	input  [7:0] red,
+	input  [7:0] green,
+	input  [7:0] blue,
 
+	output       hblank_out,
+	output       vblank_out,
+	output       hs_out,
+	output       vs_out,
 	output [7:0] red_out,
 	output [7:0] green_out,
 	output [7:0] blue_out
@@ -25,8 +32,6 @@ module cofi (
 	end
 	endfunction
 
-reg blank_last;
-
 reg [7:0] red_last;
 reg [7:0] green_last;
 reg [7:0] blue_last;
@@ -35,21 +40,31 @@ reg [7:0] red_mix;
 reg [7:0] green_mix;
 reg [7:0] blue_mix;
 
-assign red_out = enable ? red_mix : red;
-assign blue_out = enable ? blue_mix : blue;
-assign green_out = enable ? green_mix : green;
+assign red_out    = enable ? red_mix    : red;
+assign blue_out   = enable ? blue_mix   : blue;
+assign green_out  = enable ? green_mix  : green;
+
+reg hblank_mix, vblank_mix, vs_mix, hs_mix;
+
+assign hs_out     = enable ? hs_mix     : hs;
+assign vs_out     = enable ? vs_mix     : vs;
+assign hblank_out = enable ? hblank_mix : hblank;
+assign vblank_out = enable ? vblank_mix : vblank;
 
 always @(posedge clk) if (pix_ce) begin
 	
-	blank_last <= blank;
+	hblank_mix <= hblank;
+	vblank_mix <= vblank;
+	vs_mix     <= vs;
+	hs_mix     <= hs;
 
-	red_last <= red;
-	blue_last <= blue;
+	red_last   <= red;
+	blue_last  <= blue;
 	green_last <= green;
 
-	red_mix <= color_blend(red_last, red, blank_last);
-	blue_mix <= color_blend(blue_last, blue, blank_last);
-	green_mix <= color_blend(green_last, green, blank_last);
+	red_mix   <= color_blend(red_last,   red,   hblank_mix);
+	blue_mix  <= color_blend(blue_last,  blue,  hblank_mix);
+	green_mix <= color_blend(green_last, green, hblank_mix);
 	
 end
 
