82d6b89f877b7d0bb2be80114b978834f85d45a9
sorgelig <pour.garbage@gmail.com>
2019-10-24 01:49:48 +0800

    Some refactoring in blending module.

--- cofi.sv
+++ cofi.sv
@@ -13,13 +13,13 @@ module cofi (
 	input  [7:0] green,
 	input  [7:0] blue,
 
-	output       hblank_out,
-	output       vblank_out,
-	output       hs_out,
-	output       vs_out,
-	output [7:0] red_out,
-	output [7:0] green_out,
-	output [7:0] blue_out
+	output reg       hblank_out,
+	output reg       vblank_out,
+	output reg       hs_out,
+	output reg       vs_out,
+	output reg [7:0] red_out,
+	output reg [7:0] green_out,
+	output reg [7:0] blue_out
 );
 
 	function bit [7:0] color_blend (
@@ -36,37 +36,21 @@ reg [7:0] red_last;
 reg [7:0] green_last;
 reg [7:0] blue_last;
 
-reg [7:0] red_mix;
-reg [7:0] green_mix;
-reg [7:0] blue_mix;
-
-assign red_out    = enable ? red_mix    : red;
-assign blue_out   = enable ? blue_mix   : blue;
-assign green_out  = enable ? green_mix  : green;
-
-reg hblank_mix, vblank_mix, vs_mix, hs_mix;
-
-assign hs_out     = enable ? hs_mix     : hs;
-assign vs_out     = enable ? vs_mix     : vs;
-assign hblank_out = enable ? hblank_mix : hblank;
-assign vblank_out = enable ? vblank_mix : vblank;
-
 always @(posedge clk) if (pix_ce) begin
 	
-	hblank_mix <= hblank;
-	vblank_mix <= vblank;
-	vs_mix     <= vs;
-	hs_mix     <= hs;
+	hblank_out <= hblank;
+	vblank_out <= vblank;
+	vs_out     <= vs;
+	hs_out     <= hs;
 
 	red_last   <= red;
 	blue_last  <= blue;
 	green_last <= green;
 
-	red_mix   <= color_blend(red_last,   red,   hblank_mix);
-	blue_mix  <= color_blend(blue_last,  blue,  hblank_mix);
-	green_mix <= color_blend(green_last, green, hblank_mix);
-	
-end
+	red_out    <= enable ? color_blend(red_last,   red,   hblank_out) : red;
+	blue_out   <= enable ? color_blend(blue_last,  blue,  hblank_out) : blue;
+	green_out  <= enable ? color_blend(green_last, green, hblank_out) : green;
 
+end
 
 endmodule
